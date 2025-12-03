61efdae290ef1b584830eea6ef681c8da84a0548
Luca <luca.bonasegla@telekom.de>
2025-12-03 23:15:27 +0100

    fixed; BaseURl

--- quasar.config.js
+++ quasar.config.js
@@ -71,19 +71,6 @@ export default defineConfig((/* ctx */) => {
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
-    devServer: {
-      https: false,
-      port: 8080, // oder dein aktueller Quasar-Port
-      open: true,
-      proxy: {
-        '/api': {
-          target: 'http://swgoh-backend:3000',
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
-    },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
